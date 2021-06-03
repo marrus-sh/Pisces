2e85d0e5eefd2a78ab94432bd46aa5a36ff2a74e
kibigo! <marrus-sh@users.noreply.github.com>
2021-06-03 16:41:44 -0700

    E·C·M·A·262_Object tests

--- Values/Objects/E·C·M·A·262_Object.js
+++ Values/Objects/E·C·M·A·262_Object.js
@@ -1,4 +1,4 @@
-//  @ Pisces :: Objects :: E·C·M·A·262_Object.js
+//  @ Pisces :: Values :: Objects :: E·C·M·A·262_Object.js
 //
 //  Copyright © 2020–2021 Margaret KIBI (kibigo!).
 //
@@ -17,26 +17,53 @@ export default class E·C·M·A·262_Object extends E·C·M·A·262_LanguageValu
 		//  This function will always return an object; it will throw a TypeError if conversion fails.
 		if ( $ == null ) throw new TypeError (`Can’t convert ${ $ } to an object.`)
 		else return Object($) }
-	get "is extensible" ( ) { return Object.isExtensible(this) }
+	get "is extensible" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the extensibility of a primitive.")
+		else return Object.isExtensible(this) }
 	set "is extensible" ( $ ) {
-		if ( $ && !Object.isExtensible(this) ) throw new TypeError ("Can’t make non·extensible object extensible.")
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t set the extensibility of a primitive.")
+		else if ( $ && !Object.isExtensible(this) ) throw new TypeError ("Can’t make non·extensible object extensible.")
 		else if ( !$ ) Object.preventExtensions(this) }
-	get "is frozen" ( ) { return Object.isFrozen(this) }
+	get "is frozen" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the frozenness of a primitive.")
+		else return Object.isFrozen(this) }
 	set "is frozen" ( $ ) {
-		if ( !$ && Object.isFrozen(this) ) throw new TypeError ("Can’t make frozen object unfrozen.")
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t set the frozenness of a primitive.")
+		else if ( !$ && Object.isFrozen(this) ) throw new TypeError ("Can’t make frozen object unfrozen.")
 		else if ( $ ) Object.freeze(this) }
-	get "is sealed" ( ) { return Object.isSealed(this) }
+	get "is sealed" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the sealedness of a primitive.")
+		else return Object.isSealed(this) }
 	set "is sealed" ( $ ) {
-		if ( !$ && Object.isSealed(this) ) throw new TypeError ("Can’t make sealed object unsealed.")
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t set the sealedness of a primitive.")
+		else if ( !$ && Object.isSealed(this) ) throw new TypeError ("Can’t make sealed object unsealed.")
 		else if ( $ ) Object.seal(this) }
 	get "live property descriptors" ( ) { throw new TypeError ("Live property descriptor support requires E·C·M·A·262_PropertyDescriptor.") }
-	get "own entries" ( ) { return Object.entries(this) }
-	get "own entry keys" ( ) { return Object.keys(this) }
-	get "own entry values" ( ) { return Object.values(this) }
-	get "own keys" ( ) { return Reflect.ownKeys(this) }
-	get "own names" ( ) { return Object.getOwnPropertyNames(this) }
-	get "own property descriptors" ( ) { return Object.getOwnPropertyDescriptors(this) }
-	get "own symbols" ( ) { return Object.getOwnPropertySymbols(this) }
-	get prototype ( ) { return Object.getPrototypeOf(this) }
-	set prototype ( $ ) { Object.setPrototypeOf(this, $) }
-	static [Symbol.hasInstance] ( $ ) { return E·C·M·A·262_LanguageValue.get("type", result) == "object" } }
+	get "own entries" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the own entries of a primitive.")
+		else return Object.entries(this) }
+	get "own entry keys" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the own entry keys of a primitive.")
+		else return Object.keys(this) }
+	get "own entry values" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the own entry values of a primitive.")
+		else return Object.values(this) }
+	get "own keys" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the own keys of a primitive.")
+		else return Reflect.ownKeys(this) }
+	get "own names" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the own names of a primitive.")
+		else return Object.getOwnPropertyNames(this) }
+	get "own property descriptors" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the own property descriptors of a primitive.")
+		else return Object.getOwnPropertyDescriptors(this) }
+	get "own symbols" ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the own symbols of a primitive.")
+		else return Object.getOwnPropertySymbols(this) }
+	get prototype ( ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t get the prototype of a primitive.")
+		else return Object.getPrototypeOf(this) }
+	set prototype ( $ ) {
+		if ( E·C·M·A·262_LanguageValue.get("type", this) != "object" ) throw new TypeError ("Can’t set the prototype of a primitive.")
+		else Object.setPrototypeOf(this, $) }
+	static [Symbol.hasInstance] ( $ ) { return E·C·M·A·262_LanguageValue.get("type", $) == "object" } }
---
+++ Values/Objects/E·C·M·A·262_Object.test.js
@@ -0,0 +1,154 @@
+#!/usr/bin/env -S deno test
+//  @ Pisces :: Values :: Objects :: E·C·M·A·262_Object.test.js
+//
+//  Copyright © 2020–2021 Margaret KIBI (kibigo!).
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+//  If a copy of the MPL was not distributed with this file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
+
+import E·C·M·A·262_Object from "./E·C·M·A·262_Object.js"
+import { assert, assertEquals, assertStrictEquals, assertThrows } from "https://deno.land/std@0.97.0/testing/asserts.ts"
+
+const values =
+	{ boolean: true
+	, string: "ƒriŋ"
+	, symbol: Symbol()
+	, number: 23
+	, bigint: 420n
+	, object: Object.create(null)
+	, function: ( ) => { }
+	, array : [ ]
+	, proxy: new Proxy ({ }, { }) }
+
+const symbols =
+	{ enumerableSymbolAccess: Symbol("enumerable symbol access")
+	, enumerableSymbolData: Symbol("enumerable symbol data")
+	, non·enumerableSymbolAccess: Symbol("non·enumerable symbol access")
+	, non·enumerableSymbolData: Symbol("non·enumerable symbol data") }
+
+const desc =
+	{ enumerableNameAccess:
+		{ configurable: false
+		, enumerable: true
+		, get ( ) { return "enumerable name access" }
+		, set: undefined }
+	, enumerableNameData:
+		{ configurable: false
+		, enumerable: true
+		, value: "enumerable name data"
+		, writable: false }
+	, [symbols.enumerableSymbolAccess]:
+		{ configurable: false
+		, enumerable: true
+		, get ( ) { return "enumerable symbol access" }
+		, set: undefined }
+	, [symbols.enumerableSymbolData]:
+		{ configurable: false
+		, enumerable: true
+		, value: "enumerable symbol data"
+		, writable: false }
+	, non·enumerableNameAccess:
+		{ configurable: false
+		, enumerable: false
+		, get ( ) { return "non·enumerable name access" }
+		, set: undefined }
+	, non·enumerableNameData:
+		{ configurable: false
+		, enumerable: false
+		, value: "non·enumerable name data"
+		, writable: false }
+	, [symbols.non·enumerableSymbolAccess]:
+		{ configurable: false
+		, enumerable: false
+		, get ( ) { return "non·enumerable symbol access" }
+		, set: undefined }
+	, [symbols.non·enumerableSymbolData]:
+		{ configurable: false
+		, enumerable: false
+		, value: "non·enumerable symbol data"
+		, writable: false } }
+
+Deno.test(`Calling E·C·M·A·262_Object throws.`, ( ) => assertThrows(( ) => E·C·M·A·262_Object({ })))
+
+Deno.test(`Constructing E·C·M·A·262_Object returns an object when passed a non‐nullish value.`, ( ) => Object.values(values).forEach(value => {
+		assertEquals(E·C·M·A·262_Object.get("type", new E·C·M·A·262_Object (value)), "object")
+		assertStrictEquals(typeof new E·C·M·A·262_Object (value), typeof Object(value))
+		assertEquals((new E·C·M·A·262_Object (value)).valueOf?.(), Object(value).valueOf?.())
+		assertStrictEquals(Object.getPrototypeOf(new E·C·M·A·262_Object (value)), Object.getPrototypeOf(Object(value))) }))
+
+Deno.test(`Constructing E·C·M·A·262_Object throws when passed a nullish value.`, ( ) => {
+	assertThrows(( ) => new E·C·M·A·262_Object (undefined))
+	assertThrows(( ) => new E·C·M·A·262_Object (null)) })
+
+Deno.test(`E·C·M·A·262_Object :: Getting "is extensible" is correct.`, ( ) => {
+	assert(E·C·M·A·262_Object.get("is extensible", { }))
+	assert(!E·C·M·A·262_Object.get("is extensible", Object.preventExtensions({ }))) })
+
+Deno.test(`E·C·M·A·262_Object :: Setting "is extensible" is correct.`, ( ) => {
+	const obj = { }
+	E·C·M·A·262_Object.set("is extensible", true, obj)
+	assert(Object.isExtensible(obj))
+	E·C·M·A·262_Object.set("is extensible", false, obj)
+	assert(!Object.isExtensible(obj))
+	assertThrows(( ) => E·C·M·A·262_Object.set("is extensible", true, obj)) })
+
+Deno.test(`E·C·M·A·262_Object :: Getting "is frozen" is correct.`, ( ) => {
+	assert(!E·C·M·A·262_Object.get("is frozen", { }))
+	assert(E·C·M·A·262_Object.get("is frozen", Object.freeze({ }))) })
+
+Deno.test(`E·C·M·A·262_Object :: Setting "is frozen" is correct.`, ( ) => {
+	const obj = { }
+	E·C·M·A·262_Object.set("is frozen", false, obj)
+	assert(!Object.isFrozen(obj))
+	E·C·M·A·262_Object.set("is frozen", true, obj)
+	assert(Object.isFrozen(obj))
+	assertThrows(( ) => E·C·M·A·262_Object.set("is frozen", false, obj)) })
+
+Deno.test(`E·C·M·A·262_Object :: Getting "is sealed" is correct.`, ( ) => {
+	assert(!E·C·M·A·262_Object.get("is sealed", { }))
+	assert(E·C·M·A·262_Object.get("is sealed", Object.seal({ }))) })
+
+Deno.test(`E·C·M·A·262_Object :: Setting "is sealed" is correct.`, ( ) => {
+	const obj = { }
+	E·C·M·A·262_Object.set("is sealed", false, obj)
+	assert(!Object.isSealed(obj))
+	E·C·M·A·262_Object.set("is sealed", true, obj)
+	assert(Object.isSealed(obj))
+	assertThrows(( ) => E·C·M·A·262_Object.set("is sealed", false, obj)) })
+
+Deno.test(`E·C·M·A·262_Object :: Getting "own entries" returns key‐value pairs of own enumerable named properties.`, ( ) => assertEquals(new Set (E·C·M·A·262_Object.get("own entries", Object.create({ foo: "bar" }, desc))), new Set (
+	[ [ "enumerableNameAccess", "enumerable name access" ]
+	, [ "enumerableNameData", "enumerable name data" ] ])))
+
+Deno.test(`E·C·M·A·262_Object :: Getting "own entry keys" returns keys of own enumerable named properties.`, ( ) => assertEquals(new Set (E·C·M·A·262_Object.get("own entry keys", Object.create({ foo: "bar" }, desc))), new Set ([ "enumerableNameAccess", "enumerableNameData" ])))
+
+Deno.test(`E·C·M·A·262_Object :: Getting "own entry values" returns values of own enumerable named properties.`, ( ) => assertEquals(new Set (E·C·M·A·262_Object.get("own entry values", Object.create({ foo: "bar" }, desc))), new Set ([ "enumerable name access", "enumerable name data" ])))
+
+Deno.test(`E·C·M·A·262_Object :: Getting "own keys" returns keys of own properties.`, ( ) => assertEquals(new Set (E·C·M·A·262_Object.get("own keys", Object.create({ foo: "bar" }, desc))), new Set ([ "enumerableNameAccess", "enumerableNameData", symbols.enumerableSymbolAccess, symbols.enumerableSymbolData, "non·enumerableNameAccess", "non·enumerableNameData", symbols.non·enumerableSymbolAccess, symbols.non·enumerableSymbolData ])))
+
+Deno.test(`E·C·M·A·262_Object :: Getting "own names" returns keys of own named properties.`, ( ) => assertEquals(new Set (E·C·M·A·262_Object.get("own names", Object.create({ foo: "bar" }, desc))), new Set ([ "enumerableNameAccess", "enumerableNameData", "non·enumerableNameAccess", "non·enumerableNameData" ])))
+
+Deno.test(`E·C·M·A·262_Object :: Getting "own property descriptors" returns property descriptors of own properties.`, ( ) => assertEquals(E·C·M·A·262_Object.get("own property descriptors", Object.create({ foo: "bar" }, desc)), desc))
+
+Deno.test(`E·C·M·A·262_Object :: Getting "own symbols" returns keys of own symbolic properties.`, ( ) => assertEquals(new Set (E·C·M·A·262_Object.get("own symbols", Object.create({ foo: "bar" }, desc))), new Set ([ symbols.enumerableSymbolAccess, symbols.enumerableSymbolData, symbols.non·enumerableSymbolAccess, symbols.non·enumerableSymbolData ])))
+
+Deno.test(`E·C·M·A·262_Object :: Getting "prototype" is correct.`, ( ) => {
+	assertStrictEquals(E·C·M·A·262_Object.get("prototype", { }), Object.prototype)
+	assertStrictEquals(E·C·M·A·262_Object.get("prototype", Object.create(null)), null)
+	assertStrictEquals(E·C·M·A·262_Object.get("prototype", Object.create(Number.prototype)), Number.prototype)
+	assertThrows(( ) => E·C·M·A·262_Object.get("prototype", 1)) })
+
+Deno.test(`E·C·M·A·262_Object :: Setting "prototype" is correct.`, ( ) => {
+	const obj = { }
+	E·C·M·A·262_Object.set("prototype", Number.prototype, obj)
+	assertStrictEquals(Object.getPrototypeOf(obj), Number.prototype)
+	E·C·M·A·262_Object.set("prototype", null, obj)
+	assertStrictEquals(Object.getPrototypeOf(obj), null)
+	assertThrows(( ) => E·C·M·A·262_Object.set("prototype", 2, obj))
+	Object.preventExtensions(obj)
+	assertThrows(( ) => E·C·M·A·262_Object.set("prototype", Object.prototype, obj))
+	assertThrows(( ) => E·C·M·A·262_Object.set("prototype", Object.prototype, 2)) })
+
+Deno.test(`instanceof E·C·M·A·262_Object returns true for objects.`, ( ) => [ { }, [ ], ( ) => { }, new Proxy ({ }, { }) ].forEach(value => assert(value instanceof E·C·M·A·262_Object)))
+
+Deno.test(`instanceof E·C·M·A·262_Object returns false for primitives.`, ( ) => [ undefined, null, true, "string", Symbol(), 27, 98n ].forEach(value => assert(!(value instanceof E·C·M·A·262_Object))))
